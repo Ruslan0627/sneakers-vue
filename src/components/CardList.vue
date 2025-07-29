1411164a3378c2f0831b04b513864d576f83eb95
Ruslan0627 <mamedovruslan627@gmail.com>
2025-07-29 18:23:47 +0300

    create-filter-price-name

--- src/components/CardList.vue
+++ src/components/CardList.vue
@@ -1,10 +1,19 @@
 <script setup>
 import Card from "./Card.vue";
+defineProps({
+	items:Array
+})
 </script>
 <template>
 	<div class="p-10">
 		<div class="grid grid-cols-3 gap-4">
-			<Card :price="2000" title="Nike Blazer" image-url="/sneakers/sneakers-3.jpg" />
+			<Card 
+			v-for="item in items"
+			:key="item.id"
+			:title="item.title"
+			:price="item.price" 
+			:imageUrl="item.imageUrl"
+			 />
 		</div>
 	</div>
 </template>
